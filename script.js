0f3479a7022435f9447d5286f0b330da19661efa
JoeHuang99 <55128981+JoeHuang99@users.noreply.github.com>
2022-07-25 16:16:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var btn = document.getElementById("contact-github");
+btn.addEventListener("click", function() {
+    this.style.color = "white";
+    window.open('https://github.com/JoeHuang99?tab=repositories');
+})
